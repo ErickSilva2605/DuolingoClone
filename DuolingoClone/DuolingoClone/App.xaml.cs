5ac06dacc2d939b2a261f551b06ef760b99d0316
Erick Augusto <erick.dev.it@gmail.com>
2020-03-16 23:43:35 -0300

    Configurando Prims

--- DuolingoClone/DuolingoClone/App.xaml.cs
+++ DuolingoClone/DuolingoClone/App.xaml.cs
@@ -1,16 +1,31 @@
-﻿using System;
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace DuolingoClone
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : this(initializer, true) { }
+
+        public App(IPlatformInitializer initializer, bool setFormsDependencyResolver) : base(initializer, setFormsDependencyResolver) { }
+
+
+        protected override async void OnInitialized()
         {
             InitializeComponent();
+            await NavigationService.NavigateAsync("MainPage");
+        }
 
-            MainPage = new MainPage();
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
         }
 
         protected override void OnStart()
