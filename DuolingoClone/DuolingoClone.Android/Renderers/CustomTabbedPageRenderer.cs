c8686090460a05119aff42071caef58d8dfbc455
Erick Augusto <erick.dev.it@gmail.com>
2020-03-17 23:36:39 -0300

    Custumizando a TabbedPage

--- DuolingoClone/DuolingoClone.Android/Renderers/CustomTabbedPageRenderer.cs
+++ DuolingoClone/DuolingoClone.Android/Renderers/CustomTabbedPageRenderer.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
 using Android.Content;
-using Android.OS;
-using Android.Runtime;
+using Android.Support.Design.BottomNavigation;
 using Android.Support.Design.Widget;
-using Android.Views;
-using Android.Widget;
 using DuolingoClone.Droid.Renderers;
+using DuolingoClone.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using TabPagRenderer = Xamarin.Forms.Platform.Android.AppCompat;
@@ -20,6 +13,10 @@ namespace DuolingoClone.Droid.Renderers
 {
     public class CustomTabbedPageRenderer : TabPagRenderer.TabbedPageRenderer
     {
+        private readonly int _minimumHeight = 220;
+        private readonly int _iconSize = 130;
+        private BottomNavigationView _bottomNavigationView;
+        private TabbedPage _formsTabs;
         public CustomTabbedPageRenderer(Context context) : base(context)
         {
         }
@@ -30,12 +27,84 @@ namespace DuolingoClone.Droid.Renderers
 
             if (e.NewElement != null)
             {
+                _formsTabs = Element;
+                _formsTabs.CurrentPageChanged += OnCurrentPageChanged;
+
                 var relativeLayout = base.GetChildAt(0) as Android.Widget.RelativeLayout;
-                var bottomNavigationView = relativeLayout.GetChildAt(1)as BottomNavigationView;
+                _bottomNavigationView = relativeLayout.GetChildAt(1) as BottomNavigationView;
+                _bottomNavigationView.ItemIconTintList = null;
+                _bottomNavigationView.ItemIconSize = _iconSize;
+                _bottomNavigationView.SetShiftMode(false, false);
+                _bottomNavigationView.LabelVisibilityMode = LabelVisibilityMode.LabelVisibilityUnlabeled;
 
-                var bottomNavigationHeight = bottomNavigationView.MinimumHeight;
+                _bottomNavigationView.SetMinimumHeight(_minimumHeight);
+                UpdateAllTabs();
+            }
 
-                bottomNavigationView.SetMinimumHeight(220);
+            if (e.OldElement != null)
+            {
+                _formsTabs.CurrentPageChanged -= OnCurrentPageChanged;
+            }
+        }
+
+        private void OnCurrentPageChanged(object sender, EventArgs e)
+        {
+            UpdateAllTabs();
+        }
+
+        private void UpdateAllTabs()
+        {
+            for (var index = 0; index < _formsTabs.Children.Count; index++)
+            {
+                var androidTab = _bottomNavigationView.Menu.GetItem(index);
+
+                if (_formsTabs.Children[index] is LessonsView)
+                {
+                    if (_formsTabs.Children[index] == _formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_lessons_selected);
+                        continue;
+                    }
+
+                    androidTab.SetIcon(Resource.Drawable.tab_lessons);
+                    continue;
+                }
+
+                if (_formsTabs.Children[index] is ProfileView)
+                {
+                    if (_formsTabs.Children[index] == _formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_profile_selected);
+                        continue;
+                    }
+
+                    androidTab.SetIcon(Resource.Drawable.tab_profile);
+                    continue;
+                }
+
+                if (_formsTabs.Children[index] is RankingView)
+                {
+                    if (_formsTabs.Children[index] == _formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_ranking_selected);
+                        continue;
+                    }
+
+                    androidTab.SetIcon(Resource.Drawable.tab_ranking);
+                    continue;
+                }
+
+                if (_formsTabs.Children[index] is StoreView)
+                {
+                    if (_formsTabs.Children[index] == _formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_store_selected);
+                        continue;
+                    }
+
+                    androidTab.SetIcon(Resource.Drawable.tab_store);
+                    continue;
+                }
             }
         }
     }
--- DuolingoClone/DuolingoClone.iOS/Renderers/CustomTabbedPageRenderer.cs
+++ DuolingoClone/DuolingoClone.iOS/Renderers/CustomTabbedPageRenderer.cs
@@ -1,5 +1,9 @@
 ﻿using CoreGraphics;
 using DuolingoClone.iOS.Renderers;
+using DuolingoClone.Views;
+using System;
+using System.Threading.Tasks;
+using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
@@ -9,6 +13,16 @@ namespace DuolingoClone.iOS.Renderers
     public class CustomTabbedPageRenderer : TabbedRenderer
     {
         private readonly float _tabBarHeight = 150f;
+        private readonly string _imageTabLessons = "tab_lessons.png";
+        private readonly string _imageTabTraining = "tab_training.png";
+        private readonly string _imageTabProfile = "tab_profile.png";
+        private readonly string _imageTabRanking = "tab_ranking.png";
+        private readonly string _imageTabStore = "tab_store.png";
+        private readonly string _imageTabLessonsSelected = "tab_lessons_selected.png";
+        private readonly string _imageTabTrainingSelected = "tab_training_selected.png";
+        private readonly string _imageTabProfileSelected = "tab_profile_selected.png";
+        private readonly string _imageTabRankingSelected = "tab_ranking_selected.png";
+        private readonly string _imageTabStoreSelected = "tab_store_selected.png";
         public override void ViewDidLayoutSubviews()
         {
             base.ViewDidLayoutSubviews();
@@ -19,6 +33,68 @@ namespace DuolingoClone.iOS.Renderers
                 TabBar.Frame.Width,
                 _tabBarHeight
             );
+        }
+
+        protected override async Task<Tuple<UIImage, UIImage>> GetIcon(Page page)
+        {
+            if (page is LessonsView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile(_imageTabLessons),
+                        GetImageFromFile(_imageTabLessonsSelected)
+                    )
+                );
+            }
+
+            if (page is TrainingView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile(_imageTabTraining),
+                        GetImageFromFile(_imageTabTrainingSelected)
+                    )
+                );
+            }
+
+            if (page is ProfileView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile(_imageTabProfile),
+                        GetImageFromFile(_imageTabProfileSelected)
+                    )
+                );
+            }
+
+            if (page is RankingView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile(_imageTabRanking),
+                        GetImageFromFile(_imageTabRankingSelected)
+                    )
+                );
+            }
+
+            if (page is StoreView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile(_imageTabStore),
+                        GetImageFromFile(_imageTabStoreSelected)
+                    )
+                );
+            }
+
+            return await base.GetIcon(page);
+        }
+
+        private UIImage GetImageFromFile(string imageName)
+        {
+            return UIImage.
+                FromFile(imageName).
+                ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
         }
     }
 }
