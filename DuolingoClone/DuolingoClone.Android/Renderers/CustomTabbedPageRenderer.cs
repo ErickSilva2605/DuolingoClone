76547ef76b852bc280dc3c190610a32ba038d1a9
Erick Augusto <erick.dev.it@gmail.com>
2020-03-17 21:47:09 -0300

    Aumentando o tamanho da TabbedPage no Android e iOS

---
+++ DuolingoClone/DuolingoClone.Android/Renderers/CustomTabbedPageRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+using DuolingoClone.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using TabPagRenderer = Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CustomTabbedPageRenderer))]
+namespace DuolingoClone.Droid.Renderers
+{
+    public class CustomTabbedPageRenderer : TabPagRenderer.TabbedPageRenderer
+    {
+        public CustomTabbedPageRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var relativeLayout = base.GetChildAt(0) as Android.Widget.RelativeLayout;
+                var bottomNavigationView = relativeLayout.GetChildAt(1)as BottomNavigationView;
+
+                var bottomNavigationHeight = bottomNavigationView.MinimumHeight;
+
+                bottomNavigationView.SetMinimumHeight(220);
+            }
+        }
+    }
+}
---
+++ DuolingoClone/DuolingoClone.iOS/Renderers/CustomTabbedPageRenderer.cs
@@ -0,0 +1,24 @@
+﻿using CoreGraphics;
+using DuolingoClone.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CustomTabbedPageRenderer))]
+namespace DuolingoClone.iOS.Renderers
+{
+    public class CustomTabbedPageRenderer : TabbedRenderer
+    {
+        private readonly float _tabBarHeight = 150f;
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+
+            TabBar.Frame = new CGRect(
+                TabBar.Frame.X,
+                TabBar.Frame.Y + (TabBar.Frame.Height - _tabBarHeight),
+                TabBar.Frame.Width,
+                _tabBarHeight
+            );
+        }
+    }
+}
