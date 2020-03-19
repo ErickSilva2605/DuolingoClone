892cd62aa53c59e65ce993a1af2501949f164ab3
Erick Augusto <erick.dev.it@gmail.com>
2020-03-18 23:05:59 -0300

    Personalizando TitleView das abas

---
+++ DuolingoClone/DuolingoClone.iOS/Renderers/CustomNavigationPageRenderer.cs
@@ -0,0 +1,29 @@
+﻿using CoreGraphics;
+using DuolingoClone.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(CustomNavigationPageRenderer))]
+namespace DuolingoClone.iOS.Renderers
+{
+    public class CustomNavigationPageRenderer : NavigationRenderer
+    {
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+
+            if (Toolbar != null)
+            {
+                Toolbar.SizeToFit();
+
+                Toolbar.Frame = new CGRect
+                {
+                    X = 0,
+                    Y = View.Bounds.Height - Toolbar.Frame.Height,
+                    Width = View.Bounds.Width,
+                    Height = Toolbar.Frame.Height
+                };
+            }
+        }
+    }
+}
