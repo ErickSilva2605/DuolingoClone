7eaa7165c029d41e907f2c3c9b894b73add9ff98
Erick Augusto <erick.dev.it@gmail.com>
2020-03-21 16:59:34 -0300

    Carregando lições na view model

--- DuolingoClone/DuolingoClone/ViewModels/LessonsViewModel.cs
+++ DuolingoClone/DuolingoClone/ViewModels/LessonsViewModel.cs
@@ -1,23 +1,41 @@
 ﻿using DuolingoClone.Interfaces;
+using DuolingoClone.Models;
 using Prism.Commands;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace DuolingoClone.ViewModels
 {
-    public class LessonsViewModel : ViewModelBase
+    public class LessonsViewModel : ViewModelBase, IInitialize
     {
+        private readonly ILessonService _lessonService;
         public ICommand NavigateToTrainingCommand { get; private set; }
+        public IList<LessonGroupModel> LessonGroups { get; private set; }
+
         public LessonsViewModel(ILessonService lessonService)
         {
+            _lessonService = lessonService;
             NavigateToTrainingCommand = new DelegateCommand(NavigateToTrainingExecute);
+            LessonGroups = new List<LessonGroupModel>();
         }
 
         private void NavigateToTrainingExecute()
         {
             System.Diagnostics.Debug.WriteLine("FAB foi selecionado");
+        }
+
+        public async void Initialize(INavigationParameters parameters)
+        {
+            LessonGroups = await GetLessonsGroup();
+        }
+
+        private async Task<IList<LessonGroupModel>> GetLessonsGroup()
+        {
+            return await _lessonService.GetLessonsGroup();
         }
     }
 }
