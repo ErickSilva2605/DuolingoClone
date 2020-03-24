620965004a4d427524fb0a156226f76257d2f682
Erick Augusto <erick.dev.it@gmail.com>
2020-03-23 23:21:40 -0300

    Adicionando converters para as lições

---
+++ DuolingoClone/DuolingoClone/Converters/LevelToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DuolingoClone.Converters
+{
+    public class LevelToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value.ToString())
+            {
+                case "":
+                    return "#C287F8";
+                case "4":
+                    return "#F19A37";
+                default:
+                    return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
