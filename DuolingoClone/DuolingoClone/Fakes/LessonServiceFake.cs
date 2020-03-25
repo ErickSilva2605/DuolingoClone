65eed3fb462e6d399d2d3294de01f4c3c098cb06
Erick Augusto <erick.dev.it@gmail.com>
2020-03-24 22:38:39 -0300

    Adicionando mais liçoes

--- DuolingoClone/DuolingoClone/Fakes/LessonServiceFake.cs
+++ DuolingoClone/DuolingoClone/Fakes/LessonServiceFake.cs
@@ -8,6 +8,14 @@ namespace DuolingoClone.Fakes
 {
     public class LessonServiceFake : ILessonService
     {
+        private readonly string _colorLevel0 = "#C287F8";
+        private readonly string _colorLevel1 = "#4FAEF0";
+        private readonly string _colorLevel2 = "#8BC63B";
+        private readonly string _colorLevel3 = "#EC5954";
+        private readonly string _colorLevel4 = "#F19A37";
+        private readonly string _colorLevel5 = "#F7C745";
+        private readonly string _colorBonus = "#FFFFFF";
+
         public async Task<IList<LessonGroupModel>> GetLessonsGroup()
         {
             return await Task.Run(() =>
@@ -19,7 +27,7 @@ namespace DuolingoClone.Fakes
                         Type = LessonGroupTypeEnum.Single,
                         Lessons = new List<LessonModel>()
                         {
-                            GetNewLesson("Introdução", "4", "lesson_egg", "#F19A37")
+                            GetNewLesson("Introdução", "5", "lesson_egg", _colorLevel5)
                         }
                      },
 
@@ -28,8 +36,8 @@ namespace DuolingoClone.Fakes
                         Type = LessonGroupTypeEnum.Multi,
                         Lessons = new List<LessonModel>()
                         {
-                            GetNewLesson("Saudações", "4", "lesson_dialog", "#F19A37"),
-                            GetNewLesson("Viagem", string.Empty, "lesson_airplane", "#C287F8")
+                            GetNewLesson("Saudações", "4", "lesson_dialog", _colorLevel4),
+                            GetNewLesson("Viagem", "3", "lesson_airplane", _colorLevel3)
                         }
                     },
 
@@ -38,8 +46,8 @@ namespace DuolingoClone.Fakes
                         Type = LessonGroupTypeEnum.Multi,
                         Lessons = new List<LessonModel>()
                         {
-                            GetNewLesson("Cafeteria", string.Empty, "lesson_hamburger", "#C287F8"),
-                            GetNewLesson("Famílias", string.Empty, "lesson_baby", "#C287F8")
+                            GetNewLesson("Cafeteria", "2", "lesson_hamburger", _colorLevel2),
+                            GetNewLesson("Famílias", "1", "lesson_baby", _colorLevel1)
                         }
                     },
 
@@ -48,9 +56,39 @@ namespace DuolingoClone.Fakes
                         Type = LessonGroupTypeEnum.Bonus,
                         Lessons = new List<LessonModel>()
                         {
-                            GetNewLesson("Bônus", string.Empty, "lesson_plus", "#FFFFFF"),
-                            GetNewLesson("Bônus", string.Empty, "lesson_plus", "#FFFFFF"),
-                            GetNewLesson("Bônus", string.Empty, "lesson_plus", "#FFFFFF")
+                            GetNewLesson("Bônus", string.Empty, "lesson_plus", _colorBonus),
+                            GetNewLesson("Bônus", string.Empty, "lesson_plus", _colorBonus),
+                            GetNewLesson("Bônus", string.Empty, "lesson_plus", _colorBonus)
+                        }
+                    },
+
+                    new LessonGroupModel
+                    {
+                        Type = LessonGroupTypeEnum.Multi,
+                        Lessons = new List<LessonModel>()
+                        {
+                            GetNewLesson("Loja", string.Empty, "lesson_sock", _colorLevel0),
+                            GetNewLesson("Estudos", "1", "lesson_pencil", _colorLevel1),
+                            GetNewLesson("Ocupações", "3", "lesson_hat", _colorLevel3)
+                        }
+                    },
+
+                    new LessonGroupModel
+                    {
+                        Type = LessonGroupTypeEnum.Single,
+                        Lessons = new List<LessonModel>()
+                        {
+                            GetNewLesson("Encontros", string.Empty, "lesson_bag", _colorLevel0)
+                        }
+                    },
+
+                    new LessonGroupModel
+                    {
+                        Type = LessonGroupTypeEnum.Multi,
+                        Lessons = new List<LessonModel>()
+                        {
+                            GetNewLesson("Rotinas", "1", "lesson_bike", _colorLevel1),
+                            GetNewLesson("Emoções", string.Empty, "lesson_heart", _colorLevel0)
                         }
                     }
                 };
