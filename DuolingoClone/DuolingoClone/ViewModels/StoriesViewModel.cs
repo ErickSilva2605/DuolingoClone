ebbc53924ba577dca42f07061f3cccc16824168e
Erick Augusto <erick.dev.it@gmail.com>
2020-03-27 23:51:20 -0300

    Implementando a StoriesViewModel para carregar a lista quando cclicar na tela, utilizando a interface IActiveAware do prims

--- DuolingoClone/DuolingoClone/ViewModels/StoriesViewModel.cs
+++ DuolingoClone/DuolingoClone/ViewModels/StoriesViewModel.cs
@@ -1,16 +1,52 @@
 ﻿using DuolingoClone.Interfaces;
+using DuolingoClone.Models;
+using Prism;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DuolingoClone.ViewModels
 {
-    public class StoriesViewModel : ViewModelBase
+    public class StoriesViewModel : ViewModelBase, IActiveAware
     {
         private readonly IStoriesService _storiesService;
+        public ObservableCollection<StoriesModel> Stories { get; private set; }
+
+        private bool _isActive;
+        public bool IsActive 
+        { 
+            get => _isActive; 
+            set => SetProperty(ref _isActive, value, RaiseIsActivatedChanged); 
+        }
+
         public StoriesViewModel(IStoriesService storiesService)
         {
             _storiesService = storiesService;
+            Stories = new ObservableCollection<StoriesModel>();
+        }
+
+        public event EventHandler IsActiveChanged;
+
+        private async void RaiseIsActivatedChanged()
+        {
+            if (IsActive)
+            {
+                var stories = await GetStories();
+
+                if (Stories.Any())
+                    Stories.Clear();
+
+                foreach (var storie in stories)
+                    Stories.Add(storie);
+            }
+        }
+
+        private async Task<IList<StoriesModel>> GetStories()
+        {
+            return await _storiesService.GetStories();
         }
     }
 }
