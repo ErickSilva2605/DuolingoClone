9b1d482c83b35fc69e07a89f5b9b929300040fc8
Erick Augusto <erick.dev.it@gmail.com>
2020-03-29 22:22:24 -0300

    Adicionando behavior para desativar o a cor do item seleecionado do list view das conquistas

---
+++ DuolingoClone/DuolingoClone/Behaviors/ListViewDeselectItemBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DuolingoClone.Behaviors
+{
+    public class ListViewDeselectItemBehavior : Behavior<ListView>
+    {
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.ItemSelected += ListViewItemSelect;
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.ItemSelected -= ListViewItemSelect;
+        }
+
+        private void ListViewItemSelect(object sender, SelectedItemChangedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
